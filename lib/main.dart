e6b209cdc5022135961f22b3394b917cfe5d990f
Dev-YashM <grindwithyash@gmail.com>
2026-02-22 01:57:36 +0530

    Removed Firebase Dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mahalaxmi_coolers/constants/notifiers.dart';
 import 'package:mahalaxmi_coolers/features/splash.dart';
 import 'core/theme/app_theme.dart';
-import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   runApp(const MyApp());
 }
 
